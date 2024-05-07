623a4aefeecb9f959ad88bdfdecd28ff08f01d24
bagussurya12 <bagus.suryaindrajati@gmail.com>
2024-05-07 15:00:18 +0700

    Add Route Update Sosmed Handler

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import AuthHandler from "../handlers/AuthHandler.js";
 import UserHandler from "../handlers/userHandler.js";
-import userHandler from "../handlers/userHandler.js";
+import SosmedHandler from "../handlers/SosmedHandler.js";
 
 const router = express.Router();
 
@@ -11,9 +11,12 @@ router.post("/login", AuthHandler.login);
 router.post("/refresh-token", AuthHandler.refreshToken);
 // USERS ROUTE
 router.get("/Users", UserHandler.getAllUserHandler);
-router.get("/users/:userId", userHandler.getUserById);
+router.get("/users/:userId", UserHandler.getUserById);
 router.get("/user/:nickName", UserHandler.getUserByNickName);
 router.post("/Add-User", UserHandler.addUserHandler);
 router.put("/Update-User/:userId", UserHandler.updateUserHandler);
-router.delete("/users/delete/:userId", userHandler.deleteUserById);
+router.delete("/users/delete/:userId", UserHandler.deleteUserById);
+// SOSMED ROUTE
+router.put("sosmed/:sosmedId", SosmedHandler.updateSosmedHandler);
+
 export default router;
