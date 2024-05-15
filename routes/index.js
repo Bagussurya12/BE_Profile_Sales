c205988b1d79c188c88598df7cbee72883bf2ece
bagussurya12 <bagus.suryaindrajati@gmail.com>
2024-05-15 16:24:12 +0700

    Add JWT Auth On Routes

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ import SosmedHandler from "../handlers/SosmedHandler.js";
 import ProductHandler from "../handlers/ProductHandler.js";
 import ProfileHandler from "../handlers/ProfileHandler.js";
 import { uploadFiles } from "../middlewares/UploadFile.js";
+import jwtAuth from "../middlewares/JwtAuth.js";
 
 const router = express.Router();
 
@@ -15,15 +16,15 @@ router.post("/refresh-token", AuthHandler.refreshToken);
 // PROFILE HANDLER
 router.put("/Profile/:userId", uploadFiles, ProfileHandler.updateProfileHandler);
 // USERS ROUTE
-router.get("/Users", UserHandler.getAllUserHandler);
-router.get("/users/:userId", UserHandler.getUserById);
-router.get("/user/:nickName", UserHandler.getUserByNickName);
-router.post("/Add-User", UserHandler.addUserHandler);
-router.put("/Update-User/:userId", UserHandler.updateUserHandler);
-router.delete("/users/delete/:userId", UserHandler.deleteUserById);
+router.get("/Users", jwtAuth(), UserHandler.getAllUserHandler);
+router.get("/users/:userId", jwtAuth(), UserHandler.getUserById);
+router.get("/user/:nickName", jwtAuth(), UserHandler.getUserByNickName);
+router.post("/Add-User", jwtAuth(), UserHandler.addUserHandler);
+router.put("/Update-User/:userId", jwtAuth(), UserHandler.updateUserHandler);
+router.delete("/users/delete/:userId", jwtAuth(), UserHandler.deleteUserById);
 // SOSMED ROUTE
-router.put("/sosmed/:sosmedId", SosmedHandler.updateSosmedHandler);
-router.get("/sosmed/:sosmedId", SosmedHandler.getSosmedById);
+router.put("/sosmed/:sosmedId", jwtAuth(), SosmedHandler.updateSosmedHandler);
+router.get("/sosmed/:sosmedId", jwtAuth(), SosmedHandler.getSosmedById);
 // PRODUCT HANDLER
 router.post("/product/:userId", uploadFiles, ProductHandler.addProductHandler);
 router.get("/product/:userId", ProductHandler.getProductHandlerByUserId);
