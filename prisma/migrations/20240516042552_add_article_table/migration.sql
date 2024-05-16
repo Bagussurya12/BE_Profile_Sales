c9cbcecf8b666dfb33b9e2b366213138a461bc71
bagussurya12 <bagus.suryaindrajati@gmail.com>
2024-05-16 11:28:14 +0700

    Add migration Add Article Table

---
+++ prisma/migrations/20240516042552_add_article_table/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "Article" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT,
+    "title" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Article_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Article" ADD CONSTRAINT "Article_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
