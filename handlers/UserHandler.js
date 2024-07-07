c4c64306e4219b943aacec670a5e830741f4a188
bagussurya12 <bagus.suryaindrajati@gmail.com>
2024-07-07 21:38:08 +0700

    Update UserHandler And Add Column Division On Table User

--- handlers/UserHandler.js
+++ handlers/UserHandler.js
@@ -16,6 +16,7 @@ class UserHandler {
         skip: skip,
         take: perPage,
       });
+
       if (!users || users.length === 0) {
         throw { code: 404, message: "USERS_NOT_FOUND" };
       }
@@ -36,9 +37,9 @@ class UserHandler {
 
   async addUserHandler(req, res) {
     try {
-      const { fullname, email, password, level, nickName } = req.body;
+      const { fullname, email, password, level, nick_name, division } = req.body;
 
-      if (!fullname || !email || !password || !level || !nickName) {
+      if (!fullname || !email || !password || !level || !nick_name || !division) {
         throw { code: 400, message: "MISSING_REQUIRED_FIELDS" };
       }
 
@@ -55,7 +56,7 @@ class UserHandler {
         throw { code: 409, message: "EMAIL_ALREADY_EXIST" };
       }
 
-      const nickNameExist = await isNickNameExist(nickName);
+      const nickNameExist = await isNickNameExist(nick_name);
       if (nickNameExist) {
         throw { code: 409, message: "NICK_NAME_ALREADY_EXIST" };
       }
@@ -76,7 +77,8 @@ class UserHandler {
           password: hash,
           status: "active",
           level: level,
-          nick_name: nickName,
+          nick_name: nick_name,
+          division: division,
           profile: {
             create: {
               id: profileId,
@@ -126,19 +128,11 @@ class UserHandler {
       if (!req.params.userId) {
         throw { code: 428, message: "USER_ID_IS_REQUIRED" };
       }
-      const { fullname, email, nickName, password, level } = req.body;
 
-      if (!fullname) {
-        throw { code: 428, message: "FULLNAME_IS_REQUIRED" };
-      }
-      if (!email) {
-        throw { code: 428, message: "EMAIL_IS_REQUIRED" };
-      }
-      if (!nickName) {
-        throw { code: 428, message: "NICK_NAME_IS_REQUIRED" };
-      }
-      if (!level) {
-        throw { code: 428, message: "LEVEL_IS_REQUIRED" };
+      const { fullname, email, nick_name, password, level, division } = req.body;
+
+      if (!fullname || !email || !nick_name || !level || !division) {
+        throw { code: 428, message: "MISSING_REQUIRED_FIELDS" };
       }
 
       const user = await prisma.user.findUnique({
@@ -149,28 +143,26 @@ class UserHandler {
         throw { code: 404, message: "USER_NOT_FOUND" };
       }
 
-      let emailExists = false;
-      let nickNameExists = false;
-
       if (email !== user.email) {
-        emailExists = await isEmailExist(email);
+        const emailExists = await isEmailExist(email);
         if (emailExists) {
-          throw { code: 409, message: "EMAIL_EXIST" };
+          throw { code: 409, message: "EMAIL_ALREADY_EXIST" };
         }
       }
 
-      if (nickName !== user.nick_name) {
-        nickNameExists = await isNickNameExist(nickName);
+      if (nick_name !== user.nick_name) {
+        const nickNameExists = await isNickNameExist(nick_name);
         if (nickNameExists) {
-          throw { code: 409, message: "NICK_NAME_EXIST" };
+          throw { code: 409, message: "NICK_NAME_ALREADY_EXIST" };
         }
       }
 
       const dataToUpdate = {
         fullname,
         email,
-        nick_name: nickName,
+        nick_name,
         level,
+        division,
       };
 
       if (password) {
@@ -196,7 +188,7 @@ class UserHandler {
       console.log(error);
       return res.status(error.code || 500).json({
         status: false,
-        message: error.message || "INTERNAL_SERVER_ERROR",
+        message: error.message || "Internal Server Error",
       });
     }
   }
@@ -206,35 +198,9 @@ class UserHandler {
       if (!req.params.userId) {
         throw { code: 428, message: "ID_IS_REQUIRED" };
       }
-      const user = await prisma.user.findUnique({
-        where: { id: req.params.userId },
-      });
-      if (!user) {
-        throw { code: 404, message: "USER_NOT_FOUND" };
-      }
-      return res.status(200).json({
-        status: true,
-        message: "GET_USER_SUCCESS",
-        user: user,
-      });
-    } catch (error) {
-      if (!error.code) {
-        error.code = 500;
-      }
-      return res.status(500).json({
-        status: false,
-        message: error.message,
-      });
-    }
-  }
-  async getUserByNickName(req, res) {
-    try {
-      if (!req.params.nickName) {
-        throw { code: 428, message: "ID_IS_REQUIRED" };
-      }
 
       const user = await prisma.user.findUnique({
-        where: { nick_name: req.params.nickName },
+        where: { id: req.params.userId },
         include: {
           profile: {
             include: {
@@ -248,11 +214,48 @@ class UserHandler {
         throw { code: 404, message: "USER_NOT_FOUND" };
       }
 
-      // Menyusun data yang diperlukan
+      return res.status(200).json({
+        status: true,
+        message: "GET_USER_SUCCESS",
+        user,
+      });
+    } catch (error) {
+      if (!error.code) {
+        error.code = 500;
+      }
+      return res.status(500).json({
+        status: false,
+        message: error.message,
+      });
+    }
+  }
+
+  async getUserByNickName(req, res) {
+    try {
+      if (!req.params.nick_name) {
+        throw { code: 428, message: "NICK_NAME_IS_REQUIRED" };
+      }
+
+      const user = await prisma.user.findUnique({
+        where: { nick_name: req.params.nick_name },
+        include: {
+          profile: {
+            include: {
+              socialMedia: true,
+            },
+          },
+        },
+      });
+
+      if (!user) {
+        throw { code: 404, message: "USER_NOT_FOUND" };
+      }
+
       const userData = {
         fullName: user.profile?.fullName,
         photos: user.profile?.profilePhoto,
         bio: user.profile?.bio,
+        division: user.division,
         socialMedia: user.profile?.socialMedia,
       };
 
@@ -261,10 +264,10 @@ class UserHandler {
         user: userData,
       });
     } catch (error) {
-      console.log(error);
       if (!error.code) {
         error.code = 500;
       }
+      console.log(error);
       return res.status(error.code).json({
         status: false,
         message: error.message,
@@ -275,22 +278,20 @@ class UserHandler {
   async deleteUserById(req, res) {
     try {
       if (!req.params.userId) {
-        throw { code: 428, message: ID_IS_REQUIRED };
+        throw { code: 428, message: "ID_IS_REQUIRED" };
       }
       const userId = req.params.userId;
 
-      // Hapus profil pengguna terlebih dahulu
       await prisma.profile.deleteMany({
         where: { userId: userId },
       });
 
-      // Setelah menghapus profil, baru hapus pengguna
       const user = await prisma.user.delete({
         where: { id: userId },
       });
 
       if (!user) {
-        throw { code: 500, message: DELETE_USER_FAILED };
+        throw { code: 500, message: "DELETE_USER_FAILED" };
       }
 
       return res.status(200).json({
